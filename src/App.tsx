a1071f41dbf5fa96773df847e71f784fc011c12a
zThiagoR <thi.magalha13@gmail.com>
2024-01-02 23:25:32 -0300

    Refactor App component structure (add footer e main)

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,30 @@ import "./page.mdoule.scss";
 
 function App() {
   return (
-    <header>
-      <h1>Object Store</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/"><span>Home</span></a>
-          </li>
-          <li>
-            <a href="/produtos"><span>Produtos</span></a>
-          </li>
-          <li>
-            <button>Criar Orçamento</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>Object Store</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/"><span>Home</span></a>
+            </li>
+            <li>
+              <a href="/produtos"><span>Produtos</span></a>
+            </li>
+            <li>
+              <button>Criar Orçamento</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+        <p>© 2024 - Object Store</p>
+      </footer>
+    </>
   );
 }
 
